80eff1acb2e93c1769ab5c4012eded052b0ac40e
sashaFj752 <sasha2015to@gmail.com>
2025-02-09 14:59:47 +0300

    47:00 откомпилировали и запустили

--- pointerVoid/README.md
+++ pointerVoid/README.md
@@ -3,3 +3,4 @@ Olga Pavlova
 ## [Как работает и не работает обобщённый указатель void * в C (Си)](https://www.youtube.com/watch?v=uakAypiG7Zo)
 
 - 32:00 создаём src/main.c
+- 47:00 откомпилировали и запустили
--- pointerVoid/src/main.c
+++ pointerVoid/src/main.c
@@ -6,6 +6,26 @@
 #undef LOG_LVL
 #define LOG_LVL LOG_LVL_ERROR
 
+void * create_str(void *);
+
 int main() {
-    return EXIT_SUCCESS;
+    char letter_1 = 'w';
+    char letter_2 = 'g';
+
+    char * w3 = (char *)create_str(&letter_1);
+    void * g3 = create_str(&letter_2);
+
+    printf("Знаем тип: %s\n", w3);
+    printf("Не знаем тип: %s\n", (char *)g3);
+
+}
+
+void * create_str(void * cv) {
+    char c = *(char *)cv; // 1
+
+    char * result  = malloc(sizeof(char) * 4);
+    result[0] = result[1] = result[2] = c; 
+    result[3] = '\0';
+
+    return (void*)result; // 3
 }
