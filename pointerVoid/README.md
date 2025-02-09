7f15ac77c3e1c45f532e3c6983c753af9e1f771b
sashaFj752 <sasha2015to@gmail.com>
2025-02-09 18:33:43 +0300

    update README.md

--- pointerVoid/README.md
+++ pointerVoid/README.md
@@ -1,6 +1,21 @@
 Olga Pavlova
 
 ## [Как работает и не работает обобщённый указатель void * в C (Си)](https://www.youtube.com/watch?v=uakAypiG7Zo)
+```
+ 32:00 создаём src/main.c
+ 47:00 откомпилировали и запустили
+ 1:07:00 добавили указатель на указатель
 
-- 32:00 создаём src/main.c
-- 47:00 откомпилировали и запустили
+
+ 1:12:25 void free_regmem(){}
+ 
+ 
+Ловим утечки памяти:
+1) компилируем 
+    clang –ggdb –std=c99 –Wall –Werror   main.c –lcrypt –lcs50 –lm –o main
+ 
+    gcc -O0 -g main.c -o main
+ 
+ 2) запускаем 
+    valgrind ./main
+ ```
--- pointerVoid/src/main.c
+++ pointerVoid/src/main.c
@@ -1,29 +1,54 @@
-// C_BEG
 #include <stdio.h>
 #include <stdlib.h>
-#include "cbase/log.h"
-
-#undef LOG_LVL
-#define LOG_LVL LOG_LVL_ERROR
 
 void * create_str(void *);
 
+static void ** _REGMEM = NULL;
+
+void free_regmem();
+
 int main() {
+    atexit(free_regmem);
+
+    _REGMEM = malloc(sizeof(void *) * 3);
+    _REGMEM[2] = NULL;
+
     char letter_1 = 'w';
     char letter_2 = 'g';
 
     char * w3 = (char *)create_str(&letter_1);
     void * g3 = create_str(&letter_2);
 
+    // _REGMEM полностью заполнен
+
     printf("Знаем тип: %s\n", w3);
     printf("Не знаем тип: %s\n", (char *)g3);
 
+    free_regmem();
+
+    return 0;
+}
+
+void free_regmem() {
+    void ** p = _REGMEM;
+    while (*p != NULL)
+    {
+        free(*p);
+        *p = NULL;
+        ++p;
+    }
+}
+
+void add_to_regmem(void * vv) {
+    static int _next = 0;
+    _REGMEM[_next++] = vv;
 }
 
 void * create_str(void * cv) {
     char c = *(char *)cv; // 1
 
     char * result  = malloc(sizeof(char) * 4);
+    add_to_regmem(result);
     result[0] = result[1] = result[2] = c; 
     result[3] = '\0';
 
